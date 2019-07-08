163c1f1d541e5c55af0651b41e64ce9b3a31b5be
Clement Julia <clement.julia13@gmail.com>
2019-07-08 16:18:51 +0100

    added check for remaining mines

--- solver.py
+++ solver.py
@@ -5,6 +5,8 @@ import random
 from PIL import ImageGrab
 import enum
 import time
+import sys
+import logging
 
 class Categories(enum.Enum):
     bomb =  0
@@ -34,6 +36,12 @@ class Solver:
             (0, 0, 0), (128, 128, 128)
         ]
 
+        self.number_coords = [(38, 126), (64, 126), (90, 126)]
+        self.number_template_index = [
+            (0, 0), (26, 0), (52, 0), (78, 0), (104, 0), 
+            (130, 0), (156, 0), (182, 0), (208, 0), (234, 0)
+        ]
+
         self.categories = {
             "bomb": cv2.imread("assets/flag.png"),
             "full": cv2.imread("assets/full.png"),
@@ -44,19 +52,42 @@ class Solver:
 
         self.playing = cv2.imread("assets/playing.png")
         self.win = cv2.imread("assets/win.png")
-
         self.box = pyautogui.locateOnScreen("assets/top_left.png")
+        self.number_template = cv2.imread("assets/numbers.png")
+
+
         self.game = None
         self.coords = []
         self.states = []
+        self.solved = []
+        self.mines_current = 0
+        self.mines_max = 0
 
     @staticmethod
     def take_screenshot():
         pyautogui.hotkey("alt", "printscreen")
         sc = ImageGrab.grabclipboard()
-        game = cv2.cvtColor(np.array(sc), cv2.COLOR_RGB2BGR)
+        game_img = cv2.cvtColor(np.array(sc), cv2.COLOR_RGB2BGR)
 
-        return game
+        return game_img
+
+    @staticmethod
+    def restart():
+        re = pyautogui.locateOnScreen("assets/win.png")
+        if not re:
+            re = pyautogui.locateOnScreen("assets/dead.png")
+
+        pyautogui.click(re.left + 16, re.top + 16, button="left")
+
+    def get_mines_amount(self):
+        number = ''
+        for x, y in self.number_coords:
+            match = cv2.matchTemplate(self.number_template, self.game[y:y+41, x:x+21], cv2.TM_CCOEFF_NORMED)
+            loc = list(zip(*np.where( match >= 0.95)[::-1]))
+
+            number += str(self.number_template_index.index(loc[0]))
+
+        return int(number)
 
     def get_updated_cubes(self):
         start = time.time()
@@ -89,9 +120,10 @@ class Solver:
                     self.coords.append((x, y))
                     self.states.append(number)
 
+        self.mines_current = self.get_mines_amount()
 
         self.coords, self.states = (list(t) for t in zip(*sorted(zip(self.coords, self.states), key=lambda k: [k[0][1], k[0][0]])))
-        print(f"Update Cubes Time Taken: {time.time() - start}")
+        logging.debug(f"Update Cubes Time Taken: %s", (time.time() - start))
 
     def is_flag(self, x, y):
         flag = cv2.matchTemplate(self.game, self.categories["bomb"], cv2.TM_CCOEFF_NORMED)
@@ -134,25 +166,17 @@ class Solver:
         if not most_left:
             neighbors_index.extend([index - 1 - row_len, index - 1, index - 1 + row_len])
 
-        filtered_neighbors = [neighbor for neighbor in neighbors_index if 0 < neighbor < (row_len * col_len)]
+        neighbors = [neighbor for neighbor in neighbors_index if 0 < neighbor < (row_len * col_len) and self.states[neighbor] in [Categories.bomb, Categories.full]]
 
-        neighbors = []
-        for neighbor in filtered_neighbors:
-            try:
-                if self.states[neighbor] in [Categories.bomb, Categories.full]:
-                    neighbors.append(neighbor)
-            except IndexError:
-                pass
-
-        print(f"Neighbors Time Taken: {time.time() - start}")
+        logging.debug(f"Neighbors Time Taken: %s", (time.time() - start))
         return neighbors
 
-    def make_decision(self, neighbors, index, change):
+    def make_a_simple_decision(self, neighbors, index, change):
         start = time.time()
 
         full_or_bomb_neighbors = [self.states[neighbor] for neighbor in neighbors if self.states[neighbor] in [Categories.full, Categories.bomb]]
         if len(full_or_bomb_neighbors) == self.states[index] and not all([neighbor == Categories.bomb for neighbor in full_or_bomb_neighbors]):
-            print("Marking a bomb")
+            logging.debug("Marking a bomb")
             for bomb in neighbors:
                 if self.states[bomb] == Categories.bomb:
                     continue
@@ -165,7 +189,7 @@ class Solver:
 
         bomb_neighbors = [self.states[neighbor] for neighbor in neighbors if self.states[neighbor] == Categories.bomb]
         if len(bomb_neighbors) == self.states[index] and any([self.states[neighbor] == Categories.full for neighbor in neighbors]):
-            print("Clicking on some cubes")
+            logging.debug("Clicking on some cubes")
             for to_click in neighbors:
                 if self.states[to_click] == Categories.full:
                     self.click_cube(*self.coords[to_click])
@@ -173,11 +197,15 @@ class Solver:
                     
             self.get_updated_cubes()
 
-        print(f"Make Decision Time Taken: {time.time()-start}")
+        logging.debug(f"Make Decision Time Taken: %s", (time.time() - start))
+        return change
+
+    def make_a_complex_decision(self, neighbors, index, change):
         return change
 
     def solve(self):
         self.get_updated_cubes()
+        self.mines_max = self.get_mines_amount()
 
         while True:
             change = False
@@ -188,14 +216,20 @@ class Solver:
                 if not self.is_playing():
                     return self.result()
 
-                neighbors = self.get_neighbors(index)
-                # print(f"Number: {self.states[index]}")
-                # print(f"Neighbors: {neighbors}")
+                if index in self.solved:
+                    continue
 
-                change = self.make_decision(neighbors, index, change) 
+                logging.debug("Resolving for %s @ %s", self.states[index], index)
+
+                neighbors = self.get_neighbors(index)
+                if not any([self.states[neighbor] == Categories.full for neighbor in neighbors]):
+                    self.solved.append(index)
+                    continue
+
+                change = self.make_a_simple_decision(neighbors, index, change) 
 
             if not change:
-                print("Out of options, picking a random cube")
+                logging.debug("Out of options, picking a random cube")
                 coord = random.choice([coord for coord in self.coords if self.states[self.coords.index(coord)] == Categories.full])
                 self.click_cube(*coord) 
                 self.get_updated_cubes()
@@ -204,11 +238,27 @@ class Solver:
 if __name__ == '__main__':
     pyautogui.hotkey("alt", "tab")
 
-    global_start = time.time()
-    solver = Solver()
-    result = solver.solve()
+    try:
+        games = int(sys.argv[1])
+    except IndexError:
+        games = 1
+        logging.basicConfig(level=logging.DEBUG)
 
-    if result:
-        print(f"Victory in {time.time() - global_start}")
-    else:
-        print("Defeat")
+    for game in range(games):
+        global_start = time.time()
+        solver = Solver()
+        result = solver.solve()
+        final = time.time() - global_start
+
+        if result:
+            message = f"Victory in {final}\n"
+        else:
+            message = f"Defeat in {final}\n"
+
+        with open("solver1.csv", "a+") as f:
+            f.write(f"{result},{final}\n")
+
+        logging.debug(message)
+
+        if not game == games - 1:
+            solver.restart()
